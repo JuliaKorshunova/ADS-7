43c5528abc119112ffa6076f23d14250b76030e6
JuliaKorshunova <116427627+JuliaKorshunova@users.noreply.github.com>
2023-04-23 16:44:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,14 +1,16 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <iostream>
+#include <string>
 
 template<typename T>
 class TPQueue {
-  private:
-  struct Item {
-    T data;
-    Item* next;
-    Item* prev;
+ private:
+    struct Item {
+        T data;
+        Item* next;
+        Item* prev;
   };
   Item* head;
   Item* tail;
@@ -19,13 +21,13 @@ class TPQueue {
         item_name->prev = nullptr;
         return item_name;
   }
-  public:
+ public:
     TPQueue() : head(nullptr), tail(nullptr) { }
     ~TPQueue() {
     void push(const T&);
     T pop();
     void print() const;
-  };
+  }
 struct SYM {
   char ch;
   int prior;
@@ -84,7 +86,7 @@ TPQueue<T>::~TPQueue() {
     while (head) {
         pop();
     }
-}  
+}
 template <typename T>
 void TPQueue<T>::print() const {
     Item* temps = head;
